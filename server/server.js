b3276cd85e3a948a69d6dabb0f4d786fd2b5a53b
malilasage <malilaclearwater@gmail.com>
2017-01-20 19:19:20 -0700

    added npm packages to html

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
+var path = require('path');
 var express = require('express');
 var server = express();
 var port = process.env.PORT || 3000;
 
-server.use(express.static('public'));
+server.use(express.static('client'));
 
 server.get('/api', function(req, res) {
   res.send('routes working');
@@ -11,3 +12,5 @@ server.get('/api', function(req, res) {
 server.listen(port, function() {
   console.log('listening on port ' + port);
 });
+
+module.exports = server;
